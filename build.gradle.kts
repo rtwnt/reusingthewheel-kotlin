886b36a27dc09116af0d3df9dd3efbc76f03543f
Piotr Rusin <contact@reusingthewheel.net>
2020-11-19 23:57:31 +0100

    Add kotlinx.html as a dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
 
     implementation("com.vladsch.flexmark:flexmark:0.62.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
 
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
