c7a289a156003acaa4e99435d6a594a769b25d41
rtwnt <contact@reusingthewheel.net>
2022-08-07 20:13:03 +0200

    Add content parsing example

--- src/main/kotlin/net/reusinthewheel/reusingthewheelKotlin/App.kt
+++ src/main/kotlin/net/reusinthewheel/reusingthewheelKotlin/App.kt
@@ -3,6 +3,14 @@
  */
 package net.reusinthewheel.reusingthewheelKotlin
 
+import com.vladsch.flexmark.ext.yaml.front.matter.AbstractYamlFrontMatterVisitor
+import com.vladsch.flexmark.ext.yaml.front.matter.YamlFrontMatterExtension
+import com.vladsch.flexmark.html.HtmlRenderer
+import com.vladsch.flexmark.parser.Parser
+import com.vladsch.flexmark.util.data.MutableDataSet
+import java.io.File
+
+
 class App {
     val greeting: String
         get() {
@@ -11,5 +19,16 @@ class App {
 }
 
 fun main(args: Array<String>) {
-    println(App().greeting)
+    val options = MutableDataSet()
+    options.set(Parser.EXTENSIONS, listOf(YamlFrontMatterExtension.create()));
+    val parser: Parser = Parser.builder(options).build()
+    val renderer = HtmlRenderer.builder(options).build()
+
+    val document = parser.parse(File("content/posts/a-new-blog-engine-project.md").readText(Charsets.UTF_8))
+    val visitor = AbstractYamlFrontMatterVisitor()
+    visitor.visit(document)
+    println(visitor.data)
+    val html = renderer.render(document)
+
+    println(html)
 }
